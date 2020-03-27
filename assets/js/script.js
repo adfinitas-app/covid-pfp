1931b4ddf6209c154b6a0ca1559911e389607ccc
thibaut trouve <thibaut.trouve@epitech.eu>
2020-03-27 14:16:29 -0700

    Retour client, mobile -> hide content, click to expand. No borders on post-content, js on button modify the amount

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,68 @@
 // CLICK TO EXPAND
-const arrowElem = document.getElementById('arrow-down');
-const contentElem = document.getElementById('click-to-expand');
+// const arrowElem = document.getElementById('arrow-down');
+// const contentElem = document.getElementById('click-to-expand');
 
-arrowElem.addEventListener('click', function() {
-    this.classList.toggle('active');
+// arrowElem.addEventListener('click', function() {
+//     this.classList.toggle('active');
     
-    if (contentElem.style.maxHeight && contentElem.style.maxHeight !== '0px') {
-        contentElem.style.maxHeight = '0px';
+//     if (contentElem.style.maxHeight && contentElem.style.maxHeight !== '0px') {
+//         contentElem.style.maxHeight = '0px';
+//     } else {
+//         contentElem.style.maxHeight = '1000px';
+//         contentElem.style.height = 'auto';
+//     }
+// })
+
+$('.arrow-down').on('click', function() {
+    $(this).toggleClass('active');
+
+    const maxHeight = $('.click-to-expand').css('max-height');
+
+    console.log(maxHeight);
+    if (maxHeight && maxHeight !== '0px' && maxHeight !== 'none') {
+        $('.click-to-expand').css('max-height', '0px')
     } else {
-        contentElem.style.maxHeight = '1000px';
-        contentElem.style.height = 'auto';
+        $('.click-to-expand').css('max-height', '1000px');
+        $('.click-to-expand').css('height', 'auto');
     }
-})
+});
 //CLICK TO EXPAND
 
 //FORM DONATION
+function replaceAction(formElement, nb) {
+    $('.btn-don-value').text(nb);
+
+    const action = formElement.attr('action');
+    const pattern = new RegExp('amount=(.+?)((?=&)|$)');
+    const [ oldAmount ] = pattern.exec(action);
+    
+    const newAction = action.replace(oldAmount, `amount=${parseInt(nb * 100)}`)
+    formElement.attr('action', newAction);
+}
+
+$('.js-btn-don').on('click', function() {
+    $('.js-btn-don').removeClass('active');
+    $(this).addClass('active');
+
+    const amount = $(this).data('amount');
+    const formElement = $(this).parents('.row').next().children('.form-donate');
+    
+    replaceAction(formElement, +amount);
+});
+
 $('.form-amount').on('input', function() {
     const val = $(this).val();
-
+    
     if (!val)
-        return false;
+    return false;
     
     const nb = Number($(this).val());
-
+    
     if (!isNaN(nb)) {
         $('.btn-don-value').text(nb);
-        const parent = $(this).parents('.form-donate')
-        const action = parent.attr('action');
-        const pattern = new RegExp('amount=(.+?)((?=&)|$)');
-        const [ oldAmount ] = pattern.exec(action);
+        const formElement = $(this).parents('.form-donate')
         
-        const newAction = action.replace(oldAmount, `amount=${parseInt(nb * 100)}`)
-        parent.attr('action', newAction);
+        replaceAction(formElement, nb);
     }
 })
 //FORM DONATION
